7ea1fbe0777cecedd0b673db133e69c7b74159e0
danielvasco29 <danielvasconcelos.reis@gmail.com>
2022-10-07 08:11:11 -0300

    teste

---
+++ prisma/migrations/20221006153520_/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "duration" INTEGER NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_name_key" ON "users"("name");
